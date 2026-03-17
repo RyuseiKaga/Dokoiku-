e09fefb5439451b39c424fb09bad57094569838b
RyuseiKaga <karo2kai@gmail.com>
2026-03-17 09:11:33 +0900

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Dokoiku-./",  // ← これ追加
   server: {
-    host: true, // スマホからLAN経由でアクセス可能にする
+    host: true,
   },
 });
